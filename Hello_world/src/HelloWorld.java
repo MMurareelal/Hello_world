d3cadc1b751b5b64bf4479702ec59ff8d66755d9
Muraree <Muraree@DESKTOP-KVJKKQE>
2017-02-06 14:07:44 +0530

    Why i have to enter a commit message

--- Hello_world/src/HelloWorld.java
+++ Hello_world/src/HelloWorld.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 {  
 	System.out.println("Hello World");
 	System.out.println("Hello Again!!!");
-	System.out.println("What the Hell is this 'git' & what to do in this ");}
+	System.out.println("What the Hell is this 'git' & what to do in this ");
+	System.out.println("Still no idea what the f**k is going on");}
 }
